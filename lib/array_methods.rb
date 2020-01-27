44c311f15abb487732924bb6d2b5fd1da033907d
Isak Kallenbach <kallenbach13@gmail.com>
2020-01-27 15:29:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ end
 
 def find_max_value(array)
   x = 0
-  array.length.time { |element| x = array[element] if  }
+  array.length.time { |element| x = array[element] if array[index] > x }
 end
 
 def find_min_value(array)
